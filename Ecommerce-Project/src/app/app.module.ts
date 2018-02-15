98a16cd141b00e61ffa104d22059029a0677fec0
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-02-15 15:04:49 +0530

    update

--- Ecommerce-Project/src/app/app.module.ts
+++ Ecommerce-Project/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
+import { LoginComponent } from './login-component/login-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
