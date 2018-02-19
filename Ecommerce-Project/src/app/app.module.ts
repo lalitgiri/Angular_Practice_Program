cd8e8dc83f6e7b94804a161878129dc8850de7c0
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-02-19 16:30:36 +0530

    update

--- Ecommerce-Project/src/app/app.module.ts
+++ Ecommerce-Project/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { RouterModule,Routes} from '@angular/router';
 
 import {HttpModule} from '@angular/http';
 import { HomeComponent } from './home/home.component';
+import {CarouselModule } from 'angular4-carousel';
+
 const appRoutes: Routes = [
  
 ];
@@ -20,7 +22,7 @@ const appRoutes: Routes = [
     HomeComponent
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot([{ path: 'login', component: LoginComponent }]),HttpModule  ],
+    BrowserModule,RouterModule.forRoot([{ path: 'login', component: LoginComponent }]),HttpModule,CarouselModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Ecommerce-Project/src/app/home/home.component.ts
+++ Ecommerce-Project/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { animation } from '@angular/animations'
+import {ICarouselConfig,AnimationConfig} from 'angular4-carousel';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,7 +11,24 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  public imagesSources:string[]=[
+    '../../assets/images/img.jpeg',
+    '../../assets/images/img1.jpeg',
+    '../../assets/images/img2.jpg',
+    '../../assets/images/img3.jpg',
+    '../../assets/images/img4.jpg',
+    '../../assets/images/img5.jpg'
+  ];
   ngOnInit() {
   }
 
+  public config: ICarouselConfig={
+    verifyBeforeLoad:true,
+    log:false,
+    animation:true,
+    animationType:AnimationConfig.SLIDE,
+    autoplay:true,
+    autoplayDelay:2000,
+    stopAutoplayMinWidth:768
+  };
 }
