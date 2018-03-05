e59e68dc1d7286c718ffb8ec64f11fb69c408b3d
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-03-05 17:59:19 +0530

    update

--- Ecommerce-Project/src/app/app.module.ts
+++ Ecommerce-Project/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule,Routes} from '@angular/router';
+import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 
-import { HttpModule} from '@angular/http';
+
 import { DescriptionComponent } from './description/description.component';
 import { ViewAllComponent } from './view-all/view-all.component';
 import { AdminComponent } from './admin/admin.component';
