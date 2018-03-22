f9efe6ee83fec7982f906f23ab1541f5af49a5f2
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-03-22 18:02:05 +0530

    upload

--- image-uploading-Example-angular/src/app/app.module.ts
+++ image-uploading-Example-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './test/hello/hello.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
