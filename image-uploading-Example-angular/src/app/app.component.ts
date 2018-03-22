2440e797c60c9165459327e5576dff8de848da9a
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-03-22 15:32:43 +0530

    upload

--- image-uploading-Example-angular/src/app/app.component.ts
+++ image-uploading-Example-angular/src/app/app.component.ts
@@ -11,23 +11,25 @@ export class AppComponent {
   selectedFile:any[];
   flag=true;
   i:number=0;
+  count:number=0;
   constructor(private http :HttpClient ){}
   onFileSelected(event){
     console.log(event);
     this.selectedFile= [];
-    
-    this.selectedFile[0]=<File>event.target.files[0];
-    this.selectedFile[1]=<File>event.target.files[1];
+    for(this.i=0;event.target.files[this.i]!=null;this.i++)
+      this.selectedFile[this.i]=<File>event.target.files[this.i];
+
+      debugger;
   }
   onUpload(){
     if(this.flag==true)
     {
-      for(this.i=0;this.i<2;this.i++){
+      for(this.count=0;this.count<this.i;this.count++){
       let fd = new FormData();
-      fd.append('file',this.selectedFile[this.i],this.selectedFile[this.i].name);
-      debugger;
+      fd.append('file',this.selectedFile[this.count],this.selectedFile[this.count].name);
+      
       this.http.post('http://localhost:8080/upload',fd, {responseType: 'text'}).subscribe((res)=>console.log(res));
-          console.log("hello"+this.i+"  ");
+          console.log("hello"+this.count+"  ");
        }   }
   }
 }
