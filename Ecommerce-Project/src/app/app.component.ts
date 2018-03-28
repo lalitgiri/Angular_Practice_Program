bda89b45fe758340d75f50bcd97a1bf5071d1365
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-03-28 12:50:54 +0530

    update

--- Ecommerce-Project/src/app/app.component.ts
+++ Ecommerce-Project/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, ChangeDetectorRef } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { Http,HttpModule } from '@angular/http';
+import { environment } from '../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,16 @@ import { Http,HttpModule } from '@angular/http';
 })
 export class AppComponent implements OnInit  {
 
-  constructor(private http:Http){}
+  constructor(private http:Http,private router:Router,private cdRef:ChangeDetectorRef){}
   itemData;
-
+  flag=false;
 
   ngOnInit() {
-    this.http.get("../../assets/test-files/navData.json").
+    this.http.get(environment.serverUrl + "getallproductCategory").
     map(response=>response.json()).
-    subscribe(data => this.itemData=data);
+    subscribe(data => {this.itemData=data
+        this.flag=true;
+    });
   }
   
   status: boolean=false;
@@ -28,5 +32,12 @@ export class AppComponent implements OnInit  {
   }
   hide(event){
     this.status=false;
+  }
+  navigate(category:string){
+      console.log(this.router.navigate(['/viewAll',category]));
+      console.log(category);
+  }
+  ngAfterViewChecked() {
+    this.cdRef.detectChanges();
   }
 }
