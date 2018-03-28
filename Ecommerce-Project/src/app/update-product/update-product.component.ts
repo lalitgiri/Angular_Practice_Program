bbebd5baa716fe947028b89ecda98765333d19e1
lalitgiri <lalitgiri1711@gmail.com>
2018-03-29 01:38:59 +0530

    view image in modify product updated

--- Ecommerce-Project/src/app/update-product/update-product.component.ts
+++ Ecommerce-Project/src/app/update-product/update-product.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Http,Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { environment } from '../../environments/environment';
-import { FormGroup,FormControl } from '@angular/forms'
+import { FormGroup, FormControl } from '@angular/forms'
 @Component({
   selector: 'app-update-product',
   templateUrl: './update-product.component.html',
@@ -14,39 +14,40 @@ export class UpdateProductComponent implements OnInit {
   selectedFile: any;
   itemData: any;
   data;
- 
+
+  imgFlag=false;
   productid;
-  UproductName; 
-  UproductCategory; 
-  UproductQuantity; 
-  UproductPrice; 
-  description ;
+  UproductName;
+  UproductCategory;
+  UproductQuantity;
+  UproductPrice;
+  description;
+  viewImage;
   imgUrl;
-  constructor(private httpClient: HttpClient,private http:Http) { }
-  url=environment.serverUrl;
+  constructor(private httpClient: HttpClient, private http: Http) { }
+  url = environment.serverUrl;
   ngOnInit() {
     this.setTable();
     this.updateProductForm = new FormGroup({
-      productid : new FormControl({value:"",disabled:true}),
-      UproductName : new FormControl(""),
-      UproductCategory : new FormControl(""),
-      UproductQuantity : new FormControl(""),
-      UproductPrice : new FormControl(""),
-      description : new FormControl("")
+      productid: new FormControl({ value: "", disabled: true }),
+      UproductName: new FormControl(""),
+      UproductCategory: new FormControl(""),
+      UproductQuantity: new FormControl(""),
+      UproductPrice: new FormControl(""),
+      description: new FormControl("")
     });
   }
 
   onSelect(data) {
-     this.productid=data.productId;
-     this.UproductName=data.productName; 
-     this.UproductCategory=data.productCategory; 
-     this.UproductQuantity=data.productQuantity; 
-     this.UproductPrice=data.productPrice; 
-     this.description=data.description;
-     this.imgUrl=data.imageUrl;
-     console.log(data);
-      console.log(this.imgUrl);
-   }
+    this.productid = data.productId;
+    this.UproductName = data.productName;
+    this.UproductCategory = data.productCategory;
+    this.UproductQuantity = data.productQuantity;
+    this.UproductPrice = data.productPrice;
+    this.description = data.description;
+    this.imgUrl = data.imageUrl;
+
+  }
   setTable() {
     this.http.get(environment.serverUrl + "getallproduct").
       map(response => response.json()).
@@ -58,28 +59,33 @@ export class UpdateProductComponent implements OnInit {
     this.selectedFile = <File>event.target.files[0];
     let fd = new FormData();
     fd.append('file', this.selectedFile, this.selectedFile.name);
-    this.httpClient.post(environment.serverUrl + 'upload/' + this.UproductCategory, fd, { responseType: 'text' })
-      .subscribe((res) => {
+    this.httpClient.post(environment.serverUrl + 'upload/' + this.UproductCategory,
+     fd, { responseType: 'text' }).subscribe((res) => {
         this.imgUrl = res;
-        console.log(this.imgUrl);
+
       });
   }
-
-  onUpdate(formValue){
+  imageViewFunction(path){
+    this.viewImage=this.url+'images/'+path;
+    this.imgFlag=true;
+  }
+  onUpdate(formValue) {
     this.data =
-    {
-      "productId":this.productid,
-      "productName": formValue.UproductName,
-      "productCategory": formValue.UproductCategory,
-      "productQuantity": formValue.UproductQuantity,
-      "productPrice": formValue.UproductPrice,
-      "description": formValue.description,
-      "imageUrl": this.imgUrl,
-      "cartId": null
-    };
-   console.log(this.data);
-   this.httpClient.post(environment.serverUrl + '/updateproduct/'+this.productid, this.data, { responseType: 'text' })
-   .subscribe(res=>{alert(res)
-    this.setTable()});
+      {
+        "productId": this.productid,
+        "productName": formValue.UproductName,
+        "productCategory": formValue.UproductCategory,
+        "productQuantity": formValue.UproductQuantity,
+        "productPrice": formValue.UproductPrice,
+        "description": formValue.description,
+        "imageUrl": this.imgUrl,
+        "cartId": null
+      };
+
+    this.httpClient.post(environment.serverUrl + '/updateproduct/' + this.productid, this.data, { responseType: 'text' })
+      .subscribe(res => {
+        alert(res)
+        this.setTable()
+      });
   }
 }
