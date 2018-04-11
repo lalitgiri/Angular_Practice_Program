1cd8cf6c6b6552bab998d9beca305b9a2fdfd65f
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2018-04-11 17:06:31 +0530

    update

--- Ecommerce-Project/src/app/components/cart/cart.component.ts
+++ Ecommerce-Project/src/app/components/cart/cart.component.ts
@@ -12,29 +12,30 @@ export class CartComponent implements OnInit {
 
   itemData;
   productData;
-  imgurl;
+  imgurl=environment.serverUrl;;
   parsedToken = this.tokenDecoder.decodeToken(sessionStorage.getItem("token"));
   id=this.parsedToken.userId;
+  name=this.parsedToken.userName;
+  address;
   p;
   flag=false;
+ 
   constructor(private http:Http,private tokenDecoder: TokenDecoderService) {
     console.log("hello id:"+this.id);
-    this.imgurl=environment.serverUrl;
     this.http.get(environment.serverUrl + "getcart/" + this.id).
     map(response => response.json()).
     subscribe(data => {
       this.itemData = data
       this.productData=this.itemData.productId;
+      this.address=this.itemData.deliveryAddress;
       console.log(this.productData);
       this.flag=true;
-    
     },(error:Error)=>{ alert(error.message)});
+    
    }
   
-  
+    
   ngOnInit() {
-   
-    }
-
-
+ 
+}
 }
